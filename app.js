c445586ad71271f9436f1dae3f4faa4be68f60f1
Tom Fox <tfox2@hotmail.co.uk>
2020-03-26 12:46:31 +0000

    Change CORS allowed origin to Glitch

--- app.js
+++ app.js
@@ -12,7 +12,10 @@ app.use(logger('dev'));
 
 app.use(function(req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader(
+    'Access-Control-Allow-Origin',
+    'https://tfox121-thesubredditgame.glitch.me'
+  );
 
   // Request methods you wish to allow
   res.setHeader(
