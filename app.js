80b37bda8dee078c4afe07620109b3cb900c5d12
Tom Fox <tfox2@hotmail.co.uk>
2020-03-28 16:35:42 +0000

    Adjust CORS Allow-Origin to fix error

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(function(req, res, next) {
   // Website you wish to allow to connect
   res.setHeader(
     'Access-Control-Allow-Origin',
-    'https://subreddit-game.herokuapp.com//'
+    'https://subreddit-game.herokuapp.com'
   );
 
   if (process.env.NODE_ENV !== 'production') {
