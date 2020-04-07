467b5eb4fea04c2e97d0445a34c41740d0b8fc43
Tom Fox <tfox2@hotmail.co.uk>
2020-04-07 19:14:01 +0100

    Add new models and stores for IP logging and guess logging.
    IPs are now logged to database whenever a websocket client connects.
    
    All guesses are now logged to the database as well.

---
+++ models/connection.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const GameSchema = new mongoose.Schema({
+  timeCreated: { type: Date, default: Date.now },
+  gameId: { type: String, required: true }
+});
+
+const ConnectionSchema = new mongoose.Schema(
+  {
+    firstAccessed: { type: Date, default: Date.now },
+    lastAccessed: { type: Date, default: Date.now },
+    ipAddress: { type: String },
+    games: [GameSchema]
+  },
+  { collection: 'connections' }
+);
+
+const Connection = mongoose.model(
+  'Connection',
+  ConnectionSchema,
+  'connections'
+);
+
+module.exports = Connection;
